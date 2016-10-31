polcak/beba-switch

OFPAT_EXP_SET_DATA_VAR part 1

udatapath/dp_exp.c
@@ -96,6 +96,21 @@ dp_exp_action(struct packet *pkt, struct ofl_action_experimenter *act) {
                 pkt->dp->global_state = global_state;
                 break;
             }
+            case (OFPAT_EXP_SET_DATA_VAR): 
+            {
+                struct ofl_exp_action_set_data_variable *act = (struct ofl_exp_action_set_data_variable *)action;
+                struct state_table *st = pkt->dp->pipeline->tables[act->table_id]->state_table;
+                
+                if (state_table_is_stateful(st)){
+                    //Here we check if state_table_is_configured() only if output/operands are flow data variables
+                    VLOG_DBG_RL(LOG_MODULE, &rl, "executing action SET DATA VAR at stage %u", act->table_id);
+                    state_table_set_data_variable(st, act, pkt);
+                }
+                else{
+                    VLOG_DBG_RL(LOG_MODULE, &rl, "ERROR SET DATA VAR at stage %u: stage not stateful", act->table_id);
+                }
+                break;
+            }
             default:
                 VLOG_WARN_RL(LOG_MODULE, &rl, "Trying to execute unknown experimenter action (%u).", htonl(act->experimenter_id));
                 break;

nikchez01/ofsoftswitch13_v2

udatapath/dp_exp.c
@@ -113,6 +113,21 @@ dp_exp_action(struct packet *pkt, struct ofl_action_experimenter *act) {
                 }
                 break;
             }
+            case (OFPAT_EXP_SET_DATA_VAR): 
+            {
+                struct ofl_exp_action_set_data_variable *act = (struct ofl_exp_action_set_data_variable *)action;
+                struct state_table *st = pkt->dp->pipeline->tables[act->table_id]->state_table;
+                
+                if (state_table_is_enabled(st)){
+                    //Here we check if state_table_is_configured() only if output/operands are flow data variables
+                    VLOG_DBG_RL(LOG_MODULE, &rl, "executing action SET DATA VAR at stage %u", act->table_id);
+                    state_table_set_data_variable(st, act, pkt);
+                }
+                else{
+                    VLOG_DBG_RL(LOG_MODULE, &rl, "ERROR SET DATA VAR at stage %u: stage not stateful", act->table_id);
+                }
+                break;
+            }
             default:
                 VLOG_WARN_RL(LOG_MODULE, &rl, "Trying to execute unknown experimenter action (%u).", htonl(act->experimenter_id));
                 break;